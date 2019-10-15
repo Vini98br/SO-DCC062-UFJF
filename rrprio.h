07b0b34d694ab8bee1040af1aa506295ed458e29
codebucket <dev1@codejr.com.br>
2019-10-15 10:20:13 -0300

    att

--- rrprio.h
+++ rrprio.h
@@ -30,14 +30,24 @@ typedef struct rrp_params {
 //Retorna o numero do slot obtido no registro do algoritmo junto ao escalonador
 int rrpInitSchedInfo();
 
+//Retorna o proximo processo a obter a CPU, conforme o algortimo RRPrio
 Process* rrpSchedule(Process *plist);
 
+//Inicializa os parametros de escalonamento de um processo p. Funcao chamada
+//normalmente quando o processo deve ser associado ao algoritmo RRPrio
 void rrpInitSchedParams(Process *p, void *rrparams);
 
+//Libera os parametros de escalonamento de um processo p. Funcao chamada
+//normalmente quando o processo e' desassociado do slot de RRPrio
+//Retorna o numero do slot ao qual o processo estava associado
 int rrpReleaseParams(Process *p);
 
+//Modifica a prioridade atual de um processo
+//E' a funcao de setSomeFeatureFn() de RRPrio
 void rrpSetPrio(Process *p, void *rrparams);
 
+//Notifica a mudanca de status de um processo para possivel manutencao de dados
+//internos ao algoritmo RRPrio, responsavel pelo processo
 void rrpNotifyProcessStatus(Process *p, int oldstatus);
 
 
